4a08253ad59c65ebcd56b13ff58a25724021432c
Matt Clough <matt.clough@mobelux.com>
2017-03-20 12:40:03 -0400

    add babelcli and build

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-class ComparisonSlider {
-	constructor(elem, opts) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+exports.default = basaBasa;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var BasaBasa = function () {
+	function BasaBasa(elem, opts) {
+		_classCallCheck(this, BasaBasa);
+
 		// Set up state
 		this.state = {
 			dimensions: {},
@@ -10,7 +24,7 @@ class ComparisonSlider {
 				width: 50,
 				height: 100
 			}
-		}
+		};
 		this.options = opts;
 
 		// Bind event handlers
@@ -37,7 +51,7 @@ class ComparisonSlider {
 		// Create handle element
 		this.handle = document.createElement('div');
 		this.handle.classList.add('basabasa__handle');
-		this.handle.setAttribute('class', `${this.handle.getAttribute('class')} ${this.options.handleClass}`);
+		this.handle.setAttribute('class', this.handle.getAttribute('class') + ' ' + this.options.handleClass);
 
 		// Create shade element
 		this.shade = document.createElement('div');
@@ -64,122 +78,153 @@ class ComparisonSlider {
 		this.handle.addEventListener('touchstart', this.handleSliderDragStart);
 
 		// Set initial position of shade
-		this.shade.style.width = `${this.state.percent.width}%`;
+		this.shade.style.width = this.state.percent.width + '%';
 
-		if ((this.underImage.complete && this.underImage.naturalWidth) && (this.overImage.complete && this.overImage.naturalWidth)) {
+		if (this.underImage.complete && this.underImage.naturalWidth && this.overImage.complete && this.overImage.naturalWidth) {
 			this.init(elem);
 		}
 	}
 
-	init(elem) {
-		// Replace the original thing
-		elem.innerHTML = '';
-		elem.appendChild(this.wrapper);
-		elem.imageCompare = this;
+	_createClass(BasaBasa, [{
+		key: 'init',
+		value: function init(elem) {
+			// Replace the original thing
+			elem.innerHTML = '';
+			elem.appendChild(this.wrapper);
+			elem.imageCompare = this;
 
-		// Set underImage to 100% width
-		this.underImage.style.cssText = 'display: block; width: 100%;';
-		this.overImage.style.cssText = `display: block; width: ${window.getComputedStyle(this.underImage).width}; height: 100%; object-fit: cover;`;
+			// Set underImage to 100% width
+			this.underImage.style.cssText = 'display: block; width: 100%;';
+			this.overImage.style.cssText = 'display: block; width: ' + window.getComputedStyle(this.underImage).width + '; height: 100%; object-fit: cover;';
 
-		// Set padding for handle overflow
-		const { width: handleWidth } = window.getComputedStyle(this.handle);
-		const handlePadding = parseInt(handleWidth)/ 2;
-		this.wrapper.style.cssText = `padding-left: ${handlePadding}px; padding-right: ${handlePadding}px`;
+			// Set padding for handle overflow
 
-		// Setup window resize listeners
-		window.addEventListener('resize', this.handleWindowResize);
-	}
+			var _window$getComputedSt = window.getComputedStyle(this.handle),
+			    handleWidth = _window$getComputedSt.width;
 
-	setSliderDimensions() {
-		const { width, height } = window.getComputedStyle(this.elem);
-		this.state.dimensions = {
-			width: parseInt(width, 10),
-			height: parseInt(height, 10)
+			var handlePadding = parseInt(handleWidth) / 2;
+			this.wrapper.style.cssText = 'padding-left: ' + handlePadding + 'px; padding-right: ' + handlePadding + 'px';
+
+			// Setup window resize listeners
+			window.addEventListener('resize', this.handleWindowResize);
 		}
-	}
+	}, {
+		key: 'setSliderDimensions',
+		value: function setSliderDimensions() {
+			var _window$getComputedSt2 = window.getComputedStyle(this.elem),
+			    width = _window$getComputedSt2.width,
+			    height = _window$getComputedSt2.height;
 
-	setSliderBounds() {
-		const { left, top } = this.elem.getBoundingClientRect();
-		const { width, height } = window.getComputedStyle(this.elem);
-		const bounds = {
-			left,
-			right: left + parseInt(width, 10),
-			top,
-			bottom: top + parseInt(height, 10)
-		};
-		this.state.sliderBounds = bounds;
-	}
-
-	handleWindowResize() {
-		this.setSliderBounds();
-		// Set underImage to 100% width
-		this.overImage.style.width = window.getComputedStyle(this.underImage).width;
-	}
-
-	handleSliderDragStart(e) {
-		const clientX = e.clientX || e.touches[0].clientX;
-		const clientY = e.clientY || e.touches[0].clientY;
-		// Set the bounds and dimensions of the slider on the screen
-		this.setSliderBounds();
-		this.setSliderDimensions();
-		// Update state
-		this.state.dragging = true;
-		this.state.dragOrigin = { x: clientX, y: clientY };
-		// Setup drag listeners on document
-		this.setupDragListeners();
-	}
-
-	setupDragListeners() {
-		document.addEventListener('mousemove', this.handleSliderDrag);
-		document.addEventListener('touchmove', this.handleSliderDrag);
-		document.addEventListener('mouseup', this.handleSliderDragEnd);
-		document.addEventListener('touchend', this.handleSliderDragEnd);
-	}
-
-	handleSliderDragEnd() {
-		this.state.dragging = false;
-		this.state.percent = this.state.latestPercent;
-		this.state.latestPercent = 0;
-		document.removeEventListener('mousemove', this.handleSliderDrag);
-		document.removeEventListener('touchmove', this.handleSliderDrag);
-		document.removeEventListener('mouseup', this.handleSliderDragEnd);
-		document.removeEventListener('touchend', this.handleSliderDragEnd);
-	}
-
-	handleSliderDrag(e) {
-		e.stopPropagation();
-		e.preventDefault();
-		const clientX = e.clientX || e.touches[0].clientX;
-		const clientY = e.clientY || e.touches[0].clientY;
-		const { width, height } = this.state.dimensions;
-		const { x: ogX, y: ogY } = this.state.dragOrigin;
-		const { width: pWidth, height: pHeight } = this.state.percent;
-		const diff = {
-			x: ((ogX - clientX) / width) * 100,
-			y: ((ogY - clientY) / height) * 100
+			this.state.dimensions = {
+				width: parseInt(width, 10),
+				height: parseInt(height, 10)
+			};
 		}
-		const percent = {
-			width: Math.max(0, Math.min(pWidth - diff.x, 100)),
-			height: Math.max(0, Math.min(pHeight - diff.y))
-		};
-		this.state.latestPercent = percent;
-		this.shade.style.width = `${percent.width}%`;
-	}
-}
+	}, {
+		key: 'setSliderBounds',
+		value: function setSliderBounds() {
+			var _elem$getBoundingClie = this.elem.getBoundingClientRect(),
+			    left = _elem$getBoundingClie.left,
+			    top = _elem$getBoundingClie.top;
 
-export default function basabasa(selector, options) {
-	let elements;
-	if (typeof(selector) === 'string') {
+			var _window$getComputedSt3 = window.getComputedStyle(this.elem),
+			    width = _window$getComputedSt3.width,
+			    height = _window$getComputedSt3.height;
+
+			var bounds = {
+				left: left,
+				right: left + parseInt(width, 10),
+				top: top,
+				bottom: top + parseInt(height, 10)
+			};
+			this.state.sliderBounds = bounds;
+		}
+	}, {
+		key: 'handleWindowResize',
+		value: function handleWindowResize() {
+			this.setSliderBounds();
+			// Set underImage to 100% width
+			this.overImage.style.width = window.getComputedStyle(this.underImage).width;
+		}
+	}, {
+		key: 'handleSliderDragStart',
+		value: function handleSliderDragStart(e) {
+			var clientX = e.clientX || e.touches[0].clientX;
+			var clientY = e.clientY || e.touches[0].clientY;
+			// Set the bounds and dimensions of the slider on the screen
+			this.setSliderBounds();
+			this.setSliderDimensions();
+			// Update state
+			this.state.dragging = true;
+			this.state.dragOrigin = { x: clientX, y: clientY };
+			// Setup drag listeners on document
+			this.setupDragListeners();
+		}
+	}, {
+		key: 'setupDragListeners',
+		value: function setupDragListeners() {
+			document.addEventListener('mousemove', this.handleSliderDrag);
+			document.addEventListener('touchmove', this.handleSliderDrag);
+			document.addEventListener('mouseup', this.handleSliderDragEnd);
+			document.addEventListener('touchend', this.handleSliderDragEnd);
+		}
+	}, {
+		key: 'handleSliderDragEnd',
+		value: function handleSliderDragEnd() {
+			this.state.dragging = false;
+			this.state.percent = this.state.latestPercent;
+			this.state.latestPercent = 0;
+			document.removeEventListener('mousemove', this.handleSliderDrag);
+			document.removeEventListener('touchmove', this.handleSliderDrag);
+			document.removeEventListener('mouseup', this.handleSliderDragEnd);
+			document.removeEventListener('touchend', this.handleSliderDragEnd);
+		}
+	}, {
+		key: 'handleSliderDrag',
+		value: function handleSliderDrag(e) {
+			e.stopPropagation();
+			e.preventDefault();
+			var clientX = e.clientX || e.touches[0].clientX;
+			var clientY = e.clientY || e.touches[0].clientY;
+			var _state$dimensions = this.state.dimensions,
+			    width = _state$dimensions.width,
+			    height = _state$dimensions.height;
+			var _state$dragOrigin = this.state.dragOrigin,
+			    ogX = _state$dragOrigin.x,
+			    ogY = _state$dragOrigin.y;
+			var _state$percent = this.state.percent,
+			    pWidth = _state$percent.width,
+			    pHeight = _state$percent.height;
+
+			var diff = {
+				x: (ogX - clientX) / width * 100,
+				y: (ogY - clientY) / height * 100
+			};
+			var percent = {
+				width: Math.max(0, Math.min(pWidth - diff.x, 100)),
+				height: Math.max(0, Math.min(pHeight - diff.y))
+			};
+			this.state.latestPercent = percent;
+			this.shade.style.width = percent.width + '%';
+		}
+	}]);
+
+	return BasaBasa;
+}();
+
+function basaBasa(selector, options) {
+	var elements = void 0;
+	if (typeof selector === 'string') {
 		elements = document.querySelectorAll(selector);
 	} else if (selector instanceof HTMLElement) {
 		elements = [selector];
 	} else if (selector instanceof NodeList) {
 		elements = Array.from(selector);
 	} else {
-		console.log(`Argument "${selector}" isn't a valid argument for basabasa`);
+		console.log('Argument "' + selector + '" isn\'t a valid argument for BasaBasa');
 	}
 
-	elements.forEach((slider) => {
-		new ComparisonSlider(slider, options);
+	elements.forEach(function (slider) {
+		new BasaBasa(slider, options);
 	});
 }
